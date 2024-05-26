d7a22b5eae71a4fa9090063f318502aa7c9f22e0
SZtamjan <tamjan001@gmail.com>
2024-05-26 22:17:40 +0200

    Added New Input system, TextMeshPro and added interactions

---
+++ Assets/Scripts/Player/Interact/InteractableObj.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Player.Interact
+{
+    public class InteractableObj : MonoBehaviour
+    {
+        public void StartThisOnInteract()
+        {
+            Debug.Log("Interacted!");
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace UI
+{
+    public class UIController : MonoBehaviour
+    {
+        public static UIController Instance;
+
+        [SerializeField] private TextMeshProUGUI interactInfo;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        public void SwitchInteractInfoDisplay()
+        {
+            if (interactInfo.gameObject.activeSelf)
+            {
+                interactInfo.gameObject.SetActive(false);
+                return;
+            }
+            
+            interactInfo.gameObject.SetActive(true);
+        }
+        
+    }
+}
