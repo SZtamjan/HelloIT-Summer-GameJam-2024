e7c8a26b439f8b3442ec04b2dfb2a302a835513a
SZtamjan <tamjan001@gmail.com>
2024-05-28 20:41:12 +0200

    Volume Settings

---
+++ Assets/Scripts/Settings/Audio/VolumeSettings.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+namespace Settings.Audio
+{
+    public class VolumeSettings : MonoBehaviour
+    {
+        [SerializeField] private AudioMixer myMixer;
+
+        [SerializeField] private Slider musicSlider;
+        [SerializeField] private Slider sfxSlider;
+
+        public void SetMusicVolume()
+        {
+            float volume = musicSlider.value;
+            myMixer.SetFloat("MusicMixer", Mathf.Log10(volume) * 20f);
+        }
+        
+        public void SetSfxVolume()
+        {
+            float volume = sfxSlider.value;
+            myMixer.SetFloat("MusicMixer", Mathf.Log10(volume) * 20f);
+        }
+        
+    }
+}
