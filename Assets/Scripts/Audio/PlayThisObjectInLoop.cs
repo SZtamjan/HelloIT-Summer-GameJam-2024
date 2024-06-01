d6b54a8a727314621ae4c9c8590937f87cc27773
SZtamjan <tamjan001@gmail.com>
2024-06-01 02:01:18 +0200

    Audio Loop

---
+++ Assets/Scripts/Audio/PlayThisObjectInLoop.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+namespace Audio
+{
+    public class PlayThisObjectInLoop : MonoBehaviour
+    {
+        [Tooltip("Numer audio z listy")]
+        [SerializeField] private int songIndex;
+        
+        [Tooltip("In seconds")]
+        [SerializeField] private float frequency = 1f;
+        private float timePassed = 0f;
+
+        private AudioObj _audioObj;
+        private AudioSource _audioSource;
+        
+        private void Start()
+        {
+            TryGetComponent(out AudioObj audioObj);
+            if (audioObj == null)
+            {
+                Debug.LogError("BRAK SKRYPTU AudioObj");
+                return;
+            }
+            _audioObj = audioObj;
+            
+            TryGetComponent(out AudioSource audioSource);
+            if (audioSource == null)
+            {
+                Debug.LogError("BRAK KOMPONENTU AudioSource");
+                return;
+            }
+            _audioSource = audioSource;
+        }
+
+        private void Update()
+        {
+            timePassed += Time.deltaTime;
+
+            if (timePassed >= frequency)
+            {
+                _audioSource.PlayOneShot(_audioObj.sound[songIndex].clip);
+            }
+        }
+    }
+}
