a2595c54aa7052e57ede813a2295ab7166170ae9
SZtamjan <tamjan001@gmail.com>
2024-06-02 16:01:33 +0200

    Reactions to UI Implementation

---
+++ Assets/Scripts/UI/ReactionToUI.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Player;
+using Player.Movement;
+using UnityEngine;
+
+namespace UI
+{
+    public class ReactionToUI : MonoBehaviour
+    {
+        public static ReactionToUI Instance;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        public void LockMouseAndMovement()
+        {
+            LockMouse();
+            LockMovement();
+        }
+        
+        public void UnlockMouseAndMovement()
+        {
+            UnlockMouse();
+            UnlockMovement();
+        }
+        
+        public void LockMouse()
+        {
+            PlayerManager.Instance.GetComponent<PlayerMovement>().MouseRotationIsOn = false;
+        }
+
+        public void UnlockMouse()
+        {
+            PlayerManager.Instance.GetComponent<PlayerMovement>().MouseRotationIsOn = true;
+        }
+
+        public void LockMovement()
+        {
+            PlayerManager.Instance.GetComponent<PlayerMovement>().PlayerMovementIsOn = false;
+        }
+
+        public void UnlockMovement()
+        {
+            PlayerManager.Instance.GetComponent<PlayerMovement>().PlayerMovementIsOn = true;
+        }
+        
+        public void LockAndHideCursor()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        
+        public void UnlockAndShowCursor()
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+    }
+}
