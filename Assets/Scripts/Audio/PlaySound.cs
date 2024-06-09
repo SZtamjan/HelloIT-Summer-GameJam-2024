39513e3af3dfab8ff51b43d697449e2d0a483e81
WUNSZakaMICHAL <michal.bieleckilc@gmail.com>
2024-06-09 23:16:47 +0200

    Sound effects

--- Assets/Scripts/Audio/PlaySound.cs
+++ Assets/Scripts/Audio/PlaySound.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClickSound : MonoBehaviour
+public class PlaySound : MonoBehaviour
 {
     public AudioSource soundPlayer;
 
