3a958c62376c5165e3f4dfa2568dcfd8ea744e80
Damian Buczek <88405026+SZtamjan@users.noreply.github.com>
2024-06-09 21:12:49 +0200

    Idea for detection looking at objs

--- Assets/Scripts/Player/Interact/InteractableObj.cs
+++ Assets/Scripts/Player/Interact/InteractableObj.cs
@@ -6,6 +6,7 @@ namespace Player.Interact
     public class InteractableObj : MonoBehaviour
     {
         public UnityEvent interactedWithObj;
+        public UnityEvent lookAtObj;
         public string CoToJest;
 
         public void StartThisOnInteract()
@@ -13,5 +14,12 @@ namespace Player.Interact
             Debug.Log("Interacted!");
             interactedWithObj.Invoke();
         }
+
+        public void OnLookAtMe()
+        {
+            Debug.Log("Player just looked at me!");
+            lookAtObj.Invoke();
+        }
+        
     }
 }
